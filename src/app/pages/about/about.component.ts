93fb9e8434ec1b44c9251527d57f62805cee7ec7
Francesco Dore <francescodore27@gmail.com>
2024-11-21 09:25:30 +0100

    Refactored component organization

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { InstagramPostComponent } from '../../shared/components/instagram-post/instagram-post.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [InstagramPostComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
---
+++ src/app/shared/components/instagram-post/instagram-post.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-instagram-post',
+  standalone: true,
+  imports: [],
+  templateUrl: './instagram-post.component.html',
+})
+export class InstagramPostComponent {
+
+}

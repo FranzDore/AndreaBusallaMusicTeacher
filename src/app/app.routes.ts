5b3f1c7c54f1d04da33d7532540ac786a5bdb5a3
Francesco Dore <francescodore27@gmail.com>
2024-12-24 21:27:48 +0100

    Stub of About Page. Using assets instead of http urls for placeholders

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ContactsPageComponent } from './pages/contacts-page/contacts-page.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const paths = {
 	HOME: 'home',
 	ABOUT: 'about',
 	CONTACTS: 'contacts',
 }
+
+// Website is very light, no need to lazy load components :)
+// This way we provide smooth animations and navigation
 
 export const routes: Routes = [
 	{
@@ -14,18 +21,18 @@ export const routes: Routes = [
 	},
 	{
 		path: 'home',
-		loadComponent: () => import('./pages/homepage/homepage.component').then(m => m.HomepageComponent)
+		component: HomepageComponent
 	},
 	{
 		path: 'about',
-		loadComponent: () => import('./pages/about-page/about-page.component').then(m => m.AboutPageComponent)
+		component: AboutPageComponent
 	},
 	{
 		path: 'contacts',
-		loadComponent: () => import('./pages/contacts-page/contacts-page.component').then(m => m.ContactsPageComponent)
+		component: ContactsPageComponent
 	},
 	{
 		path: '**',
-		loadComponent: () => import('./pages/not-found/not-found.component').then(m => m.NotFoundComponent)
+		component: NotFoundComponent
 	}
 ];

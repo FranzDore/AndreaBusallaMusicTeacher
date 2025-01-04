2e0e425f58c95e1ab0fb1060b294db674d2aa29a
Francesco Dore <francescodore27@gmail.com>
2025-01-04 11:32:36 +0100

    Footer stub

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { SidenavComponent } from './shared/components/sidenav/sidenav.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [NavbarComponent, SidenavComponent, RouterModule],
+	imports: [NavbarComponent, FooterComponent, SidenavComponent, RouterModule],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.css'
 })
